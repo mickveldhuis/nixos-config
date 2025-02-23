19cbe1b6de6859e0e7bdfe73f96a162dc9d09d75
Mick Veldhuis <mickveldhuis@hotmail.nl>
2025-02-23 21:54:59 +0100

    Fix zsh autosuggestion typo

--- home/shells.nix
+++ home/shells.nix
@@ -21,7 +21,7 @@ in
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     
     shellAliases = aliases;

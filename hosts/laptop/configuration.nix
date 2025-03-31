621d4fd62a3645572a8be42907233890d7ba2149
Mick Veldhuis <mickveldhuis@hotmail.nl>
2025-03-31 20:54:46 +0200

    Remove network proxy stuff

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -13,14 +13,8 @@
       ../../system
     ];
 
-
-
   networking.hostName = "nix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;

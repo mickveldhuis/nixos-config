711f02d2823c54d0cfb0777b3af5f74dc797386c
Mick Veldhuis <mickveldhuis@hotmail.nl>
2025-03-31 20:52:20 +0200

    Remove explicit locale settings from hosts/laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -13,9 +13,7 @@
       ../../system
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+
 
   networking.hostName = "nix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -29,24 +27,6 @@
 
   # Enable network manager applet
   programs.nm-applet.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_IDENTIFICATION = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_TIME = "nl_NL.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
--- system/bootloader.nix
+++ system/bootloader.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Use Grub as the default bootloader.
+  # Use Grub as the default bootloader for all hosts.
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
@@ -12,6 +12,8 @@
       efiSupport = true;
       devices = [ "nodev" ];
     };
+    # Want to use systemd-boot instead? Set the following to `true`.
+    # TODO: add logic to enable GRUB of systemd-boot based on the host!
     systemd-boot.enable = false;
   };
 }
